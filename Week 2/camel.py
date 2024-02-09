5e4dfc74b05120b1b290fa8245cd09dc607f25f9
typicaleoxx <lsneha991@gmail.com>
2024-02-09 22:14:38 +0545

    updates

--- Week 2/camel.py
+++ Week 2/camel.py
@@ -1,13 +1,19 @@
-#implement a progam that prompts user for name
-#of a variable in camel case and outputs in snake case
+# implement a progam that prompts user for name
+# of a variable in camel case and outputs in snake case
 def main():
-    user_name=str(input("Enter a variable name in camel case: "))
+    user_name = str(input("Enter a variable name in camel case: "))
     snake_case(user_name)
+
+
 def snake_case(name):
     for i in name:
         if i.isupper():
-            print("_"+ i.lower(),end="")#using end"" so that it doesn't print in new line everytime
+            print(
+                "_" + i.lower(), end=""
+            )  # using end"" so that it doesn't print in new line everytime
         else:
-            print(i,end="")#end makes it print in same line
+            print(i, end="")  # end makes it print in same line
+
+
 main()
-    
+# TODO MAKE IT MORE RESPONSIVE
--- Week 2/coke.py
+++ Week 2/coke.py
@@ -1,20 +1,23 @@
-#A bottle of coke costs 50 cents
-#only accept coins of: 25,10,5
-#implement a program that prompt a user to insert a coin
-#inform amount due each time
-#after user put total of 50 cents, print due
-#assume user input only integers and ignore any integer that isn't 25,10,5
+# A bottle of coke costs 50 cents
+# only accept coins of: 25,10,5
+# implement a program that prompt a user to insert a coin
+# inform amount due each time
+# after user put total of 50 cents, print due
+# assume user input only integers and ignore any integer that isn't 25,10,5
 def main():
     while True:
-        user_coin=int(input("Insert the coin(25,10,5): "))
+        user_coin = int(input("Insert the coin(25,10,5): "))
         print(f"Your due amount :{amount_due(user_coin)}")
-        if amount_due(user_coin)!=0:
+        if amount_due(user_coin) != 0:
             continue
+
+
 def amount_due(cents):
-    if (cents==25 or cents==10) or cents==5:
-        coin=coin+cents
-        return(50-coin)
+    if (cents == 25 or cents == 10) or cents == 5:
+        coin = coin + cents
+        return 50 - coin
     else:
-        return(50)
+        return 50
+
+
 main()
-        
